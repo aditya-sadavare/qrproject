26d0f1f6174d765d29c19cc7d414bf2097f85e4f
Aditya Sadavare <86789573+aditya-sadavare@users.noreply.github.com>
2025-08-10 00:52:10 +0530

    Update Login.jsx

--- qrFrontend/src/components/Login.jsx
+++ qrFrontend/src/components/Login.jsx
@@ -15,8 +15,8 @@ const Login = () => {
   function handleSubmit(e) {
     e.preventDefault();
     const url = isLogin
-      ? "https://dynamic-qr-server.vercel.app/login"
-      : "https://dynamic-qr-server.vercel.app/reg";
+      ? `${process.env.REACT_APP_BACKEND_URL}/login`
+      : `${process.env.REACT_APP_BACKEND_URL}/reg`;
     const payload = isLogin ? { username, pass } : { username, pass, name };
 
     axios
